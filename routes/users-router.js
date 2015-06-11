0a77e81c9ff8fe7e347e42edcec4bd220a0aaf3b
Evan Turner <evturn@gmail.com>
2015-06-10 22:36:40 -0400

    created route to user profile

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var userOne = {username: 'dougmuffinz', name: 'Doug Dougsby', avatar: 'assets/img/swirl.gif'};
 
 var users = express.Router();
 
 users.route('/')
   .get(function(req, res) {
-    var user = { username: 'dougmuffinz', name: 'Doug Dougsby', avatar: 'assets/img/swirl.gif'};
-    res.render('users/index', {user: user});
+    res.render('users/index', {user: userOne});
+  });
+
+users.route('/:username')
+  .get(function(req, res) {
+    res.render('users/show', {user: userOne})
   });
 
 module.exports = users;
